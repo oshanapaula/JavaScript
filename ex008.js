c35a6aa2e2f3e46198fc9eb77a142a522dfdb9a3
Oshana <oshanadev@gmail.com>
2023-03-18 10:22:39 -0300

    Add files via upload

---
+++ ex008.js
@@ -0,0 +1,17 @@
+// CONDIÇÃO COMPOSTA
+var vel = 100
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+  console.log(`Você ultrapassou a velocidade permitida: MULTADO`)
+}
+else {
+  console.log(`Se for dirigir não beba! Dirija sempre usando o cinto de segurança`)
+}
+ 
+
+//CONDIÇÃO SIMPLES
+ var vel = 100
+ console.log(`A velocidade do seu carro é ${vel}Km/h`)
+ if (vel > 60) {
+   console.log(`Você ultrapassou a velocidade permitida: MULTADO`)
+ }
