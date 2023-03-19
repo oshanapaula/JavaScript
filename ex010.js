492c372f42b16a300d13c046ba82a399ed06de39
Oshana <oshanadev@gmail.com>
2023-03-19 16:27:44 -0300

    Add files via upload

---
+++ ex010.js
@@ -0,0 +1,10 @@
+//CONDIÇÃO ANINHADA
+var idade = 12
+console.log (`Você tem ${idade} anos.`)
+if (idade < 16) {
+  console.log('Não Vota')
+} else if (idade < 18 || idade > 65) {
+  console.log('Voto opcional')
+} else {
+  console.log('Voto obrigatório')
+}
